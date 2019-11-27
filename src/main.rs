23da468ee9c70aaa69a19e2712421d886f85381f
Peter Gagliardi <ptrgags@gmail.com>
2019-11-27 09:07:27 -0500

    Prototype writing header file for point clouds

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 extern crate rand;
+#[macro_use]
+extern crate json;
+
 use rand::Rng;
+use std::fs::File;
+use std::io::prelude::*;
 
 type Vector = (f32, f32, f32);
+type Color = (u8, u8, u8);
+type VectorList = Vec<Vector>;
+type ColorList = Vec<Color>;
 
 trait Transform {
     fn transform(&self, vector: &Vector) -> Vector;
@@ -30,12 +38,69 @@ impl Transform for SierpinskiTransform {
 }
 
 
+/*
 fn print_point(vector: &Vector, index: u32) {
     const SCALE: f32 = 10000000.0;
     let (x, y, z) = vector;
     let shade = index % 256;
     let shade2 = (index + 64) % 256;
     println!("{},{},{},{},{},{}", x * SCALE, y * SCALE, z * SCALE, 0, shade, shade2);
+}
+*/
+
+fn write_pnts(fname: &str, positions: &VectorList, colors: &ColorList) 
+        -> std::io::Result<()> {
+
+    let num_positions = positions.len() as u32;
+    const FLOAT_SIZE: u32 = 4;
+    const POSITION_SIZE: u32 = 3 * FLOAT_SIZE;
+    const COLOR_SIZE: u32 = 3;
+    let positions_length = num_positions * POSITION_SIZE;
+    let colors_length = num_positions * COLOR_SIZE;
+    let feature_table_binary_length = positions_length + colors_length;
+    let rgb_offset = positions_length;
+
+    let feature_table = object!{
+        "POINTS_LENGTH" => num_positions,
+        "POSITION" => object!{
+            "byteOffset" => 0
+        },
+        "RGB" => object!{
+            "byteOffset" => rgb_offset
+        }
+    };
+    let feature_table_json = json::stringify(feature_table);
+    let feature_table_json_bytes: &[u8] = feature_table_json.as_bytes();
+    let feature_table_json_length = feature_table_json_bytes.len() as u32;
+
+    // byte length
+    const HEADER_LENGTH: u32 = 28;
+    const BATCH_TABLE_JSON_LENGTH: u32 = 0;
+    const BATCH_TABLE_BINARY_LENGTH: u32 = 0;
+    const BATCH_TABLE_LENGTH: u32 = BATCH_TABLE_JSON_LENGTH + BATCH_TABLE_BINARY_LENGTH;
+
+    let mut file = File::create(fname)?;
+    // magic
+    file.write_all(b"pnts")?;
+
+    // version
+    file.write_all(&1u32.to_le_bytes())?;
+
+    // Total length
+    let total_length = HEADER_LENGTH + feature_table_json_length + feature_table_binary_length + BATCH_TABLE_LENGTH;
+    file.write_all(&total_length.to_le_bytes())?;
+
+    // feature table JSON length
+    file.write_all(&feature_table_json_length.to_le_bytes())?;
+
+    // Feature table binary length
+    file.write_all(&feature_table_binary_length.to_le_bytes())?;
+
+    // This doesn't use the batch table feature so set both json/binary length to 0
+    file.write_all(&BATCH_TABLE_JSON_LENGTH.to_le_bytes())?;
+    file.write_all(&BATCH_TABLE_BINARY_LENGTH.to_le_bytes())?;
+
+    Ok(())
 }
 
 fn main() {
@@ -57,13 +122,19 @@ fn main() {
     );
 
     let mut current_point = start_point;
+    let mut points: VectorList = Vec::new();
+    let mut colors: ColorList = Vec::new();
     for i in 0..TOTAL_ITERS {
         if i < DISCARD_ITERS {
             continue;
         }
 
-        print_point(&current_point, i);
+        points.push(current_point);
+        colors.push((0u8, (i % 256) as u8, (i % 256 + 64) as u8));
+
         let index = rng.gen_range(0usize, 4usize);
         current_point = functions[index].transform(&current_point);
     }
+
+    write_pnts("test.pnts", &points, &colors).expect("whoops, something went wrong");
 }
