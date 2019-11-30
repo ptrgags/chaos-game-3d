6f52d565a90c03edb1d0d1f2cb3953d075818789
Peter Gagliardi <ptrgags@gmail.com>
2019-11-30 10:30:45 -0500

    Set up viewer page
    Debugging this will be a pain

---
+++ viewer/main.js
@@ -0,0 +1,13 @@
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0ZTVjMjExYy0wYWVlLTRmNzktODM4Ni0zNzRlMjdjZDIxZmMiLCJpZCI6MTg5MTksInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1NzQ2ODg1MzJ9.aeY06JtwkvYi5MylE_cJd8QveIxvjUIb-E4HtGJ6gbg';
+
+const viewer = new Cesium.Viewer('cesiumContainer', {
+    globe: false
+});
+
+const tileset = viewer.scene.primitives.add(
+    new Cesium.Cesium3DTileset({
+        url: 'tileset_test/tileset.json'
+    })
+);
+
+viewer.zoomTo(tileset);
