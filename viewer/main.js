c5d3ab2e92a0501c17129203d6e59074abead0c0
Peter Gagliardi <ptrgags@gmail.com>
2020-02-08 15:09:11 -0500

    Attempt to add a unit sphere

--- viewer/main.js
+++ viewer/main.js
@@ -1,7 +1,7 @@
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0ZTVjMjExYy0wYWVlLTRmNzktODM4Ni0zNzRlMjdjZDIxZmMiLCJpZCI6MTg5MTksInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1NzQ2ODg1MzJ9.aeY06JtwkvYi5MylE_cJd8QveIxvjUIb-E4HtGJ6gbg';
 
 const defined = Cesium.defined;
-
+const Cartesian3 = Cesium.Cartesian3;
 
 const viewer = new Cesium.Viewer('cesiumContainer', {
     globe: false
@@ -18,6 +18,46 @@ function set_model(model_id) {
     viewer.scene.primitives.add(tileset);
 }
 
+// Not sure why this isn't working... 
+const BIGGER_THAN_EARTH = 1000000.0;
+let unit_sphere;
+function add_reference_geometry() {
+    /*
+    const options = {
+        name: 'Unit Sphere',
+        position: new Cartesian3(0.0, 0.0, 0.0),
+        ellipsoid: {
+            radii: new Cartesian3(
+                BIGGER_THAN_EARTH, 
+                BIGGER_THAN_EARTH, 
+                BIGGER_THAN_EARTH),
+            material: Cesium.Color.WHITE.withAlpha(0.3),
+            outline: true,
+            outlineColor: Cesium.Color.BLACK
+        }
+    };
+    const entity = new Cesium.Entity(options);
+    viewer.entities.add(entity);
+    */
+
+    /*
+    unit_sphere = viewer.entities.add(new Cesium.Entity({
+        name: 'Red sphere with black outline',
+        position: Cesium.Cartesian3(1.0, 0.0, 0.0),
+        ellipsoid: {
+            radii : new Cesium.Cartesian3(1000000.0, 1000000.0, 1000000.0),
+            material : Cesium.Color.RED,
+            outline : true,
+            outlineColor : Cesium.Color.BLACK
+        }
+    }));
+    */
+}
+
+function remove_reference_geometry() {
+    viewer.entities.removeAll();
+}
+
 const model_select = document.getElementById('model');
 model_select.addEventListener('change', (e) => {
     let model_id = e.target.value;
@@ -31,6 +71,12 @@ checkbox.addEventListener('change', (e) => {
     if (defined(tileset)) {
         tileset.pointCloudShading.attenuation = attenuation;
     }
+});
+
+const reload_button = document.getElementById('reload');
+reload_button.addEventListener('click', () => {
+    const model_id = model_select.value;
+    set_model(model_id);
 });
 
 set_model('sierpinski');
