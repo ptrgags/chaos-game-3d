3193e00208455278d30424c7904c1fb18442e38b
Peter Gagliardi <ptrgags@gmail.com>
2022-01-08 10:01:26 -0500

    Get random walk working

--- src/half_multivector.rs
+++ src/half_multivector.rs
@@ -1,8 +1,9 @@
 use std::cmp::Eq;
+use std::fmt::{Debug, Formatter, Result};
 
 use crate::vector::Vec3;
 
-#[derive(Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 enum Parity {
     Even,
     Odd
@@ -69,6 +70,7 @@ const PSEUDOSCALAR_START: usize = 0;
 #[allow(dead_code)]
 const PSEUDOSCALAR_END: usize = 1;
 const VECTOR_START: usize = 1;
+#[allow(dead_code)]
 const VECTOR_END: usize = 6;
 #[allow(dead_code)]
 const TRIVECTOR_START: usize = 6;
@@ -78,22 +80,22 @@ const TRIVECTOR_END: usize = 16;
 type ComponentLUT = [[usize; 16]; 16];
 
 const COMPONENTS_EVEN_EVEN: ComponentLUT = [
-    [1, YZPN, XZPN, XYPN, XYZN, XYZP, XY, XZ, XP, XN, YZ, YP, YN, ZP, ZN, PN],
-    [YZPN, 1, XY, XZ, XP, XN, XZPN, XYPN, XYZN, XYZP, PN, ZN, ZP, YN, YP, YZ],
-    [XZPN, XY, 1, YZ, YP, YN, YZPN, PN, ZN, ZP, XYPN, XYZN, XYZP, XN, XP, XZ],
-    [XYPN, XZ, YZ, 1, ZP, ZN, PN, YZPN, YN, YP, XZPN, XN, XP, XYZN, XYZP, XY],
-    [XYZN, XP, YP, ZP, 1, PN, ZN, YN, YZPN, YZ, XN, XZPN, XZ, XYPN, XY, XYZP],
-    [XYZP, XN, YN, ZN, PN, 1, ZP, YP, YZ, YZPN, XP, XZ, XZPN, XY, XYPN, XYZN],
-    [XY, XZPN, YZPN, PN, ZN, ZP, 1, YZ, YP, YN, XZ, XP, XN, XYZP, XYZN, XYPN],
-    [XZ, XYPN, PN, YZPN, YN, YP, YZ, 1, ZP, ZN, XY, XYZP, XYZN, XP, XN, XZPN],
-    [XP, XYZN, ZN, YN, YZPN, YZ, YP, ZP, 1, PN, XYZP, XY, XYPN, XZ, XZPN, XN],
-    [XN, XYZP, ZP, YP, YZ, YZPN, YN, ZN, PN, 1, XYZN, XYPN, XY, XZPN, XZ, XP],
-    [YZ, PN, XYPN, XZPN, XN, XP, XZ, XY, XYZP, XYZN, 1, ZP, ZN, YP, YN, YZPN],
-    [YP, ZN, XYZN, XN, XZPN, XZ, XP, XYZP, XY, XYPN, ZP, 1, PN, YZ, YZPN, YN],
-    [YN, ZP, XYZP, XP, XZ, XZPN, XN, XYZN, XYPN, XY, ZN, PN, 1, YZPN, YZ, YP],
-    [ZP, YN, XN, XYZN, XYPN, XY, XYZP, XP, XZ, XZPN, YP, YZ, YZPN, 1, PN, ZN],
-    [ZN, YP, XP, XYZP, XY, XYPN, XYZN, XN, XZPN, XZ, YN, YZPN, YZ, PN, 1, ZP],
-    [PN, YZ, XZ, XY, XYZP, XYZN, XYPN, XZPN, XN, XP, YZPN, YN, YP, ZN, ZP, 1],
+    [SCALAR, YZPN, XZPN, XYPN, XYZN, XYZP, XY, XZ, XP, XN, YZ, YP, YN, ZP, ZN, PN],
+    [YZPN, SCALAR, XY, XZ, XP, XN, XZPN, XYPN, XYZN, XYZP, PN, ZN, ZP, YN, YP, YZ],
+    [XZPN, XY, SCALAR, YZ, YP, YN, YZPN, PN, ZN, ZP, XYPN, XYZN, XYZP, XN, XP, XZ],
+    [XYPN, XZ, YZ, SCALAR, ZP, ZN, PN, YZPN, YN, YP, XZPN, XN, XP, XYZN, XYZP, XY],
+    [XYZN, XP, YP, ZP, SCALAR, PN, ZN, YN, YZPN, YZ, XN, XZPN, XZ, XYPN, XY, XYZP],
+    [XYZP, XN, YN, ZN, PN, SCALAR, ZP, YP, YZ, YZPN, XP, XZ, XZPN, XY, XYPN, XYZN],
+    [XY, XZPN, YZPN, PN, ZN, ZP, SCALAR, YZ, YP, YN, XZ, XP, XN, XYZP, XYZN, XYPN],
+    [XZ, XYPN, PN, YZPN, YN, YP, YZ, SCALAR, ZP, ZN, XY, XYZP, XYZN, XP, XN, XZPN],
+    [XP, XYZN, ZN, YN, YZPN, YZ, YP, ZP, SCALAR, PN, XYZP, XY, XYPN, XZ, XZPN, XN],
+    [XN, XYZP, ZP, YP, YZ, YZPN, YN, ZN, PN, SCALAR, XYZN, XYPN, XY, XZPN, XZ, XP],
+    [YZ, PN, XYPN, XZPN, XN, XP, XZ, XY, XYZP, XYZN, SCALAR, ZP, ZN, YP, YN, YZPN],
+    [YP, ZN, XYZN, XN, XZPN, XZ, XP, XYZP, XY, XYPN, ZP, SCALAR, PN, YZ, YZPN, YN],
+    [YN, ZP, XYZP, XP, XZ, XZPN, XN, XYZN, XYPN, XY, ZN, PN, SCALAR, YZPN, YZ, YP],
+    [ZP, YN, XN, XYZN, XYPN, XY, XYZP, XP, XZ, XZPN, YP, YZ, YZPN, SCALAR, PN, ZN],
+    [ZN, YP, XP, XYZP, XY, XYPN, XYZN, XN, XZPN, XZ, YN, YZPN, YZ, PN, SCALAR, ZP],
+    [PN, YZ, XZ, XY, XYZP, XYZN, XYPN, XZPN, XN, XP, YZPN, YN, YP, ZN, ZP, SCALAR],
 ];
 
 const COMPONENTS_ODD_ODD: ComponentLUT = COMPONENTS_EVEN_EVEN;
@@ -215,7 +217,21 @@ fn get_sign_table(left: &Parity, right: &Parity) -> SignLUT {
     }
 }
 
-#[derive(Clone)]
+fn find_start(array: &[f64]) -> usize {
+    match array.iter().position(|&x| x != 0.0) {
+        Some(index) => index,
+        None => array.len()
+    }
+}
+
+fn find_end(array: &[f64]) -> usize {
+    match array.iter().rposition(|&x| x != 0.0) {
+        Some(index) => index + 1,
+        None => 0
+    }
+}
+
+#[derive(Clone, PartialEq)]
 pub struct HalfMultivector {
     components: [f64; 16],
     parity: Parity,
@@ -224,6 +240,33 @@ pub struct HalfMultivector {
 }
 
 impl HalfMultivector {
+    #[allow(dead_code)]
+    pub fn even(
+            components: [f64; 16],
+            start_index: usize,
+            end_index: usize) -> Self {
+        Self {
+            components,
+            parity: Parity::Even,
+            start_index,
+            end_index
+        }
+    }
+
+    /// Create an arbitrary odd vector. Used in unit tests
+    #[allow(dead_code)]
+    pub fn odd(
+            components: [f64; 16],
+            start_index: usize,
+            end_index: usize) -> Self {
+        Self {
+            components,
+            parity: Parity::Odd,
+            start_index,
+            end_index
+        }
+    }
+
     pub fn identity() -> Self {
         let mut components = [0.0; 16];
         components[SCALAR] = 1.0;
@@ -322,7 +365,7 @@ impl HalfMultivector {
         
         // x + 1/2x^2 inf + origin
         // x + 1/2x^2 (n + p) + 1/2(n - p)
-        // x + 1/2(x^2 + 1) n + 1/2(x^2 - 1)p
+        // x + 1/2(x^2 + 1)n + 1/2(x^2 - 1)p
         let n = 0.5 * (mag_sqr + 1.0);
         let p = 0.5 * (mag_sqr - 1.0);
 
@@ -335,17 +378,9 @@ impl HalfMultivector {
         Self {
             components,
             parity: Parity::Odd,
-            start_index: VECTOR_START,
-            end_index: VECTOR_END
+            start_index: find_start(&components),
+            end_index: find_end(&components)
         }
-    }
-
-    pub fn from_vec3(position: &Vec3) -> Self {
-        Self::point(
-            *position.x() as f64,
-            *position.y() as f64,
-            *position.z() as f64
-        )
     }
 
     pub fn reverse(&self) -> Self {
@@ -371,36 +406,59 @@ impl HalfMultivector {
         let component_table = get_component_table(&self.parity, &other.parity);
         let parity = get_product_parity(&self.parity, &other.parity);
         let mut result = [0.0; 16];
-        let mut start = BIVECTOR_END;
-        let mut end = SCALAR_START;
         for i in self.start_index..self.end_index {
             let a = self.components[i];
             for j in other.start_index..other.end_index {
                 let b = other.components[j];
                 let index = component_table[i][j];
                 let sign = sign_table[i][j] as f64;
-                result[index] = sign * a * b;
-
-                start = start.min(index);
-                end = end.max(index);
+                /*
+                // helpful for debugging
+                if a != 0.0 && b != 0.0 {
+                    println!("result[{}] = {}*{}*{} = {}, [{}][{}]", index, sign, a, b, sign * a * b, i, j);
+                }
+                */
+                result[index] += sign * a * b;
             }
         }
+
+        let start = find_start(&result);
+        let end = find_end(&result);
 
         Self {
             components: result,
             parity,
             start_index: start,
-            end_index: end + 1
+            end_index: end
         }
+    }
+
+    pub fn tighten(&mut self) {
+        self.start_index = find_start(&self.components);
+        self.end_index = find_end(&self.components);
     }
 
     pub fn sandwich_product(&self, other: &Self) -> Self {
         let reverse = self.reverse();
-        self.geometric_product(&other).geometric_product(&reverse)
+        let mut result = self.geometric_product(&other).geometric_product(&reverse);
+        result.tighten();
+        result
+    }
+
+    pub fn from_vec3(position: &Vec3) -> Self {
+        Self::point(
+            *position.x() as f64,
+            *position.y() as f64,
+            *position.z() as f64
+        )
     }
 
     // for points only
     pub fn to_vec3(&self) -> Vec3 {
+        if self.parity != Parity::Odd {
+            panic!("not a vector!");
+        }
+
         let x = self.components[X];
         let y = self.components[Y];
         let z = self.components[Z];
@@ -428,5 +486,134 @@ impl HalfMultivector {
             start_index,
             end_index
         }
+    }
+}
+
+/// Debug format: (Odd|Even)[component0, component1, ..., component15](start-end)
+impl Debug for HalfMultivector {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "{:?}{:?}({}-{})", 
+            self.parity, self.components, self.start_index, self.end_index)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_identity() {
+        let expected = HalfMultivector::even(
+            [
+                1.0, 
+                0.0, 0.0, 0.0, 0.0, 0.0, 
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            0,
+            1
+        );
+
+        assert_eq!(HalfMultivector::identity(), expected);
+    }
+
+    #[test]
+    fn test_translation() {
+        let hx = 1.0;
+        let hy = 2.0;
+        let hz = 3.0;
+        let expected = HalfMultivector::even(
+            [
+                1.0, 
+                0.0, 0.0, 0.0, 0.0, 0.0, 
+                0.0, 0.0, -hx, -hx, 0.0, -hy, -hy, -hz, -hz, 0.0,
+            ],
+            0,
+            15
+        );
+        assert_eq!(HalfMultivector::translation(2.0, 4.0, 6.0), expected);
+    }
+
+    #[test]
+    fn test_product_even_even() {
+        let left = HalfMultivector::even(
+            [
+                1.0, 
+                0.0, 0.0, 0.0, 0.0, 1.0, 
+                0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            0,
+            11
+        );
+        let right = HalfMultivector::even(
+            [
+                2.0,
+                0.0, 0.0, 2.0, 0.0, 0.0,
+                1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            0,
+            7
+        );
+        let expected = HalfMultivector::even(
+            [
+                2.0, 
+                0.0, -2.0, 2.0, 0.0, 2.0, 
+                1.0, -1.0, 0.0, 0.0, 2.0, 0.0, 0.0, -1.0, -2.0, 0.0,
+            ],
+            0,
+            15
+        );
+        assert_eq!(left.geometric_product(&right), expected);
+    }
+
+    #[test]
+    fn test_point() {
+        let expected = HalfMultivector::odd(
+            [
+                0.0,
+                1.0, 2.0, 4.0, 10.0, 11.0,
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            1,
+            6
+        );
+        assert_eq!(HalfMultivector::point(1.0, 2.0, 4.0), expected);
+    }
+
+    #[test]
+    fn test_reverse() {
+        let original = HalfMultivector::odd(
+            [
+                1.0,
+                2.0, 3.0, 4.0, 5.0, 6.0,
+                7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0,
+            ],
+            0,
+            16
+        );
+        let expected = HalfMultivector::odd(
+            [
+                1.0,
+                2.0, 3.0, 4.0, 5.0, 6.0,
+                -7.0, -8.0, -9.0, -10.0, -11.0, -12.0, -13.0, -14.0, -15.0, -16.0,
+            ],
+            0,
+            16
+        );
+        assert_eq!(original.reverse(), expected);
+    }
+
+    #[test]
+    fn test_identity_xform() {
+        let xform = HalfMultivector::identity();
+        let point = HalfMultivector::point(1.0, 2.0, 3.0);
+        assert_eq!(xform.sandwich_product(&point), point);
+    }
+
+    #[test]
+    fn test_translation_xform() {
+        let xform = HalfMultivector::translation(1.0, 2.0, 3.0);
+        let point = HalfMultivector::point(1.0, 1.0, 1.0);
+        let expected = HalfMultivector::point(2.0, 3.0, 4.0);
+        assert_eq!(xform.sandwich_product(&point), expected);
     }
 }
