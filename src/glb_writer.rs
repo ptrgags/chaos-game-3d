5f5266550c57a16c321db1e3edc9c964424a9250
Peter Gagliardi <ptrgags@gmail.com>
2022-01-09 11:11:10 -0500

    WIP flesh out glTF JSON

--- src/glb_writer.rs
+++ src/glb_writer.rs
@@ -4,21 +4,53 @@ use std::io::prelude::*;
 use json::JsonValue;
 
 use crate::buffers::OutputBuffer;
+use crate::vector::Vec3;
 
 const GLTF_VERSION: u32 = 2;
 const GLTF_POINTS: u32 = 0;
+const GLTF_FLOAT: u32 = 5126;
 
 struct BufferView {
     byte_offset: u32,
     byte_length: u32
 }
 
-struct Accessor {
-    buffer_view: u32,
-    component_type: u32,
+impl BufferView {
+    pub fn new() -> Self {
+        Self {
+            byte_offset: 0,
+            byte_length: 0
+        }
+    }
 }
 
-pub struct GlbWriter {
+struct Accessor<'a> {
+    buffer_view: u32,
+    accessor_type: &'a str,
+    component_type: u32,
+    /// minimum value of each component of the accessor. Only required for
+    /// position
+    min: Option<Vec<f32>>,
+    /// minimum value of each component of the accessor. Only required for
+    /// position
+    max: Option<Vec<f32>>,
+}
+
+impl<'a> Accessor<'a> {
+    pub fn new() -> Self {
+        Self {
+            buffer_view: 0,
+            component_type: 0,
+            accessor_type: "unknown",
+            min: None,
+            max: None
+        }
+    }
+}
+
+const SIZE_VEC3: u32 = 3 * 4;
+
+pub struct GlbWriter<'a> {
     /// Number of points in this glTF point cloud
     point_count: u32,
     /// Total length of binary glTF, including header and all chunks
@@ -28,10 +60,17 @@ pub struct GlbWriter {
     binary_chunk_length: u32,
     binary_padding_length: u32,
     buffer_length: u32,
+    accessor_position: Accessor<'a>,
+    accessor_color: Accessor<'a>,
+    buffer_view_position: BufferView,
+    buffer_view_color: BufferView,
+    buffer_view_iterations: BufferView,
+    buffer_view_point_id: BufferView,
+    buffer_view_last_xforms: BufferView,
     buffer: Vec<u8>,
 }
 
-impl GlbWriter {
+impl<'a> GlbWriter<'a> {
     pub fn new() -> Self {
         Self {
             point_count: 0,
@@ -41,12 +80,19 @@ impl GlbWriter {
             binary_chunk_length: 0,
             binary_padding_length: 0,
             buffer_length: 0,
+            accessor_position: Accessor::new(),
+            accessor_color: Accessor::new(),
+            buffer_view_position: BufferView::new(),
+            buffer_view_color: BufferView::new(),
+            buffer_view_iterations: BufferView::new(),
+            buffer_view_point_id: BufferView::new(),
+            buffer_view_last_xforms: BufferView::new(),
             buffer: vec![]
         }
     }
 
     pub fn write(&mut self, fname: &str, buffer: &OutputBuffer) {
-        self.compute_lengths(&buffer);
+        self.compute_layout(&buffer);
         let json = json::stringify(self.make_json());
 
         let error_msg = format!("Cannot open {}", fname);
@@ -56,9 +102,20 @@ impl GlbWriter {
         self.write_binary_chunk(&mut file, &self.buffer)
     }
 
-    fn compute_lengths(&mut self, buffer: &OutputBuffer) {
+    fn compute_layout(&mut self, buffer: &OutputBuffer) {
         let point_count = buffer.len() as u32;
         self.point_count = point_count;
+
+        let position_length = point_count * SIZE_VEC3;
+        self.buffer_view_position.byte_offset = 0;
+        self.buffer_view_position.byte_length = position_length;
+        self.accessor_position.buffer_view = 0;
+        self.accessor_position.accessor_type = "VEC3";
+        self.accessor_position.component_type = GLTF_FLOAT;
+
+        let (min, max) = compute_min_max(buffer.get_points());
+        self.accessor_position.min = Some(min);
+        self.accessor_position.max = Some(max);
     }
 
     fn make_json(&self) -> JsonValue {
@@ -73,12 +130,44 @@ impl GlbWriter {
                 "EXT_mesh_features" => object!{
                     "schema" => object!{
                         "classes" => object!{
-
+                            "fractal" => object!{
+                                "name" => "Fractal",
+                                "description" => "Details about the fractal",
+                                "properties" => object!{
+                                    "iterations" => object!{
+                                        "componentType" => "UINT64",
+                                        "required" => true,
+                                    },
+                                    "point_id" => object!{
+                                        "componentType" => "UINT16",
+                                        "required" => true,
+                                    },
+                                    "last_xform" => object!{
+                                        "componentType" => "UINT8",
+                                        "required" => true,
+                                    }
+                                }
+                            }
                         }
                     },
-                    "propertyTables" => object!{
-
-                    }
+                    "propertyTables" => array![
+                        object!{
+                            "name" => "fractal",
+                            "class" => "fractal",
+                            "count" => self.point_count,
+                            "properties" => object!{
+                                "iterations" => object!{
+                                    "bufferView" => 2
+                                },
+                                "point_id" => object!{
+                                    "bufferView" => 3
+                                },
+                                "last_xform" => object!{
+                                    "bufferView" => 4
+                                },
+                            }
+                        }
+                    ]
                 }
             },
             "scene" => 0,
@@ -103,6 +192,13 @@ impl GlbWriter {
                             "mode" => GLTF_POINTS,
                             "extensions" => object!{
                                 "EXT_mesh_features" => object!{
+                                    "propertyTables" => array![0],
+                                    "featureIds" => array![
+                                        object!{
+                                            "offset" => 0,
+                                            "repeat" => 1
+                                        }
+                                    ]
                                 }
                             }
                         }
@@ -113,34 +209,50 @@ impl GlbWriter {
                 object!{
                     "name" => "Positions",
                     "count" => self.point_count,
-                    "bufferView" => 0
+                    "bufferView" => self.accessor_position.buffer_view,
+                    "min" => self.accessor_position.min.as_ref().unwrap().clone(),
+                    "max" => self.accessor_position.max.as_ref().unwrap().clone(),
+                    "type" => self.accessor_position.accessor_type,
+                    "component_type" => self.accessor_position.component_type
                 },
                 object!{
                     "name" => "Colors",
                     "count" => self.point_count,
-                    "bufferView" => 1
+                    "bufferView" => self.accessor_color.buffer_view,
+                    "type" => self.accessor_color.accessor_type,
+                    "component_type" => self.accessor_color.component_type
                 }
             ],
             "bufferViews" => array![
                 object!{
                     "name" => "Positions",
-                    "buffer" => 0
+                    "buffer" => 0,
+                    "byteLength" => self.buffer_view_position.byte_length,
+                    "byteOffset" => self.buffer_view_position.byte_offset,
                 },
                 object!{
                     "name" => "Colors",
-                    "buffer" => 0
+                    "buffer" => 0,
+                    "byteLength" => self.buffer_view_color.byte_length,
+                    "byteOffset" => self.buffer_view_color.byte_offset,
                 },
                 object!{
                     "name" => "Iteration Count",
-                    "buffer" => 0
+                    "buffer" => 0,
+                    "byteLength" => self.buffer_view_iterations.byte_length,
+                    "byteOffset" => self.buffer_view_iterations.byte_offset,
                 },
                 object!{
                     "name" => "Point ID",
-                    "buffer" => 0
+                    "buffer" => 0,
+                    "byteLength" => self.buffer_view_point_id.byte_length,
+                    "byteOffset" => self.buffer_view_point_id.byte_offset,
                 },
                 object!{
                     "name" => "Last xform ID",
-                    "buffer" => 0
+                    "buffer" => 0,
+                    "byteLength" => self.buffer_view_last_xforms.byte_length,
+                    "byteOffset" => self.buffer_view_last_xforms.byte_offset,
                 }
             ],
             "buffers" => array![
@@ -181,4 +293,22 @@ impl GlbWriter {
         // 0x20 is the space character
         (0..byte_len).map(|_| pad_char).collect()
     }
+}
+
+fn compute_min_max(positions: &Vec<Vec3>) -> (Vec<f32>, Vec<f32>) {
+    let mut min = [f32::MAX; 3];
+    let mut max = [f32::MIN; 3];
+    for position in positions.iter() {
+        let x = *position.x();
+        let y = *position.y();
+        let z = *position.z();
+        min[0] = min[0].min(x);
+        min[1] = min[1].min(y);
+        min[2] = min[2].min(z);
+        max[0] = max[0].max(x);
+        max[1] = max[1].max(y);
+        max[2] = max[2].max(z);
+    }
+
+    (min.to_vec(), max.to_vec())
 }
