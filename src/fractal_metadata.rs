4804211828c3fd91747f8d9dab0333824ce28017
Peter Gagliardi <ptrgags@gmail.com>
2022-01-16 14:11:45 -0500

    Add id, name and description to metadata

--- src/fractal_metadata.rs
+++ src/fractal_metadata.rs
@@ -1,18 +1,40 @@
 use json::JsonValue;
 
+/// Metadata about the fractal. This will be included in the tileset
+/// when generating 3D Tiles Next for styling and other purposes
 #[derive(Clone)]
 pub struct FractalMetadata {
+    /// unique ID for this fractal, corresponding to the filename
+    pub id: String,
+    /// Name of the fractal for display purposes
+    pub name: String,
+    /// Description of the fractal. Defaults to empty string
+    pub description: String,
+    /// The number of iterations
     pub iterations: u64,
+    /// How many points in the initial set
     pub initial_set_point_count: u16,
+    /// How many copies of the initial set
     pub initial_set_copies: u16,
+    /// How many transformations are in the IFS
     pub ifs_xform_count: u8,
+    /// How many transformations are in the color IFS. Default is 1 (identity)
     pub color_ifs_xform_count: u8,
+    /// The algorithm that was used
     pub algorithm: String,
+    /// how many points are stored in each octree node
     pub node_capacity: u16
 }
 
 impl FractalMetadata {
+    // Extract the metadata from the JSON parameter file
     pub fn from_json(json: &JsonValue) -> Self {
+        let id = &json["id"]
+            .as_str().expect("id must be a string");
+        let name = &json["name"]
+            .as_str().expect("name must be a string");
+        let description = &json["description"]
+            .as_str().unwrap_or("");
         let algorithm = &json["algorithm"]
             .as_str().expect("algorithm must be a string");
         let iterations = &json["iters"]
@@ -44,6 +66,9 @@ impl FractalMetadata {
         };
 
         Self {
+            id: id.to_string(),
+            name: name.to_string(),
+            description: description.to_string(),
             iterations: *iterations,
             initial_set_point_count: initial_set_point_count,
             initial_set_copies: *initial_set_copies,
@@ -54,12 +79,25 @@ impl FractalMetadata {
         }
     }
 
+    // Generate the metadata for 3DTILES_metadata
     pub fn make_extension_json(&self) -> JsonValue {
         object!{
             "schema" => object!{
                 "classes" => object!{
                     "tileset" => object!{
                         "properties" => object!{
+                            "id" => object!{
+                                "componentType" => "STRING",
+                                "semantic" => "ID"
+                            },
+                            "name" => object!{
+                                "componentType" => "STRING",
+                                "semantic" => "NAME"
+                            },
+                            "description" => object!{
+                                "componentType" => "STRING",
+                                "semantic" => "DESCRIPTION"
+                            },
                             "iterations" => object!{
                                 "componentType" => "UINT64"
                             },
@@ -88,6 +126,9 @@ impl FractalMetadata {
             "tileset" => object!{
                 "class" => "tileset",
                 "properties" => object!{
+                    "id" => self.id.clone(),
+                    "name" => self.name.clone(),
+                    "description" => self.description.clone(),
                     "iterations" => self.iterations,
                     "initial_set_point_count" => self.initial_set_point_count,
                     "initial_set_copies" => self.initial_set_copies,
