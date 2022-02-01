3a6f1e19a8d58e1ac1ac2c3fe19b5e0cc99a153f
Peter Gagliardi <ptrgags@gmail.com>
2022-02-01 08:09:14 -0500

    Add a fibonacci lattice cluster

--- src/initial_set.rs
+++ src/initial_set.rs
@@ -1,5 +1,4 @@
-
-
+use std::f64::consts::PI;
 use rand::Rng;
 use rand::prelude::ThreadRng;
 use json::JsonValue;
@@ -199,10 +198,109 @@ impl InitialSet for RandomLine {
     }
 }
 
+struct FibonacciSphere {
+    /// Center of the sphere
+    center: Vec3,
+    /// Radius of the sphere
+    radius: f64,
+    /// number of points to put on the surface of the sphere
+    num_points: usize,
+    /// The sphere starts off with a solid color
+    color: Vec3,
+}
+
+impl FibonacciSphere {
+    /// Parse a FibonacciSphere cluster from JSON of the form:
+    /// ```text
+    /// {
+    ///     "type": "sphere",
+    ///     "center": [x, y, z],
+    ///     "radius": r,
+    ///     "color": [r, g, b] // 0.0 to 1.0
+    ///     "num_points": N
+    /// }
+    /// ```
+    pub fn from_json(json: &JsonValue) -> Self {
+        let center = Vec3::from_json(&json["center"], Vec3::zero());
+        let radius = &json["radius"].as_f64().unwrap_or(1.0);
+        let color = Vec3::from_json(&json["color"], Vec3::ones());
+        let num_points = &json["num_points"]
+            .as_usize()
+            .expect("num_points must be a positive integer");
+
+        Self {
+            center,
+            radius: *radius,
+            num_points: *num_points,
+            color
+        }
+    }
+
+    to_box!(InitialSet);
+}
+
+impl InitialSet for FibonacciSphere {
+    fn generate(&mut self, set_id: u16) -> Vec<InternalPoint> {
+        // Golden ratio
+        let phi = (1.0 + (5.0f64).sqrt()) / 2.0;
+        let n = self.num_points as f64;
+        let r = self.radius;
+        let cx = *self.center.x() as f64;
+        let cy = *self.center.y() as f64;
+        let cz = *self.center.z() as f64;
+        let color = HalfMultivector::from_vec3(&self.color);
+
+        let mut lattice = Vec::new();
+
+        // Generate points based on the Fibonacci lattice.
+        // See here:
+        // http://extremelearning.com.au/how-to-evenly-distribute-points-on-a-sphere-more-effectively-than-the-canonical-fibonacci-lattice/
+        for i in 0..self.num_points {
+            let index = i as f64;
+            let u = (index / phi) % 1.0;
+            let v = index / n;
+
+            // theta in the article
+            let azimuth = 2.0 * PI * u;
+            // phi in the article
+            let zenith = (1.0 - 2.0 * v).acos();
+            let cos_aziumuth = azimuth.cos();
+            let sin_aziumuth = azimuth.sin();
+            let cos_zenith = zenith.cos();
+            let sin_zenith = zenith.sin();
+
+            // Convert from spherical to rectangular coordinates
+            let x = r * cos_aziumuth * sin_zenith + cx;
+            let y = r * sin_aziumuth * sin_zenith + cy;
+            let z = r * cos_zenith + cz;
+
+            let position = HalfMultivector::point(x, y, z);
+
+            let point = InternalPoint {
+                position,
+                color: color.clone(),
+                feature_id: set_id,
+                iteration: 0,
+                point_id: i as u16,
+                last_xform: 0,
+                last_color_xform: 0
+            };
+            lattice.push(point);
+        }
+
+        lattice
+    }
+
+    fn len(&self) -> usize {
+        self.num_points
+    }
+}
+
+
 /// Parse one of the initial set types from a JSON value of the form:
 /// ```text
 /// {
-///     "type": "box" | "line",
+///     "type": "box" | "line" | "sphere",
 ///     ...params
 /// }
 /// ```
@@ -215,6 +313,7 @@ pub fn from_json(json: &JsonValue) -> Box<dyn InitialSet> {
     match &type_id[..] {
         "box" => RandomBox::from_json(&json).to_box(),
         "line" => RandomLine::from_json(&json).to_box(),
+        "sphere" => FibonacciSphere::from_json(&json).to_box(),
         _ => panic!(
             "Initial set type {} must be one of {:?}", type_id, valid_types)
     }
